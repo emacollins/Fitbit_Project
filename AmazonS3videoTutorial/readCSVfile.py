924c64d84bf057d704949a60885c4b3ab793b12c
eric <collinseric321@gmail.com>
2022-07-09 11:51:30 -0700

    folder clean up

--- AmazonS3videoTutorial/readCSVfile.py
+++
@@ -1,9 +0,0 @@
-import csv
-outputlist=[]
-with open('empdata.csv',newline='') as csvfile:
-    reader = csv.DictReader(csvfile)
-    for row in reader:
-        print(row)
-        outputlist.append(row.values())
-
-print(outputlist)
